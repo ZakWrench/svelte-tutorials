0fcad92a0ebaafff4f5166436567f80ddff9d7b9
Zakaria Fatihi <zak.wrench@gmail.com>
2023-07-05 11:56:45 +0000

    reactivity

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,26 @@
 	let dance = "A man dances.";
 	import Nested from './Nested.svelte';
 	let parag1 = "This string contains some <strong>HTML!</strong>";
+	
+	let count = 0;
+	function increment() {
+		count += 1;
+	}
+	$: doubled = count * 2
+	/*Reactive Statements*/
+	$: {
+		console.log('the count is ${count}');
+		console.log('this will also be logged');
+		if (count >= 10){
+			alert('Count is dangerously high, resetting..');
+			count = 0;
+		}
+	}
+	let numbers = [1,2,3,4];
+	function addNumber() {
+		numbers = [...numbers, number.length + 1];
+	}
+	$: sum = numbers.reduce((total, currentNumber) => total + currentNumber, 0)
 </script>
 
 <h1>Hello {name.toUpperCase()}</h1>
@@ -25,4 +45,17 @@
 <!--HTML tags rendering-->
 <!--Warning: Svelte doesn't perform any sanitization of the expression inside{@html ...} before it gets inserted into the DOM. if it's some untrusted user content, e.g. a comment on an article, then it's critical that we manually escape it, otherwise we risk exposing our users to XSS attacks-->
 <p>{@html parag1}</p>
+
 <!--Reactivity-->
+<!--Assignment-->
+<button on:click={increment}>
+	clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
+<!--Reactive Declarations-->
+<p>{count} doubled is {doubled}</p>
+<!--Updating Arrays and Objects-->
+<p>{numbers.join(' + ')} = {sum}</p>
+<button on:click = {addNumber}>
+	Click to add incremental numbers
+</button>
